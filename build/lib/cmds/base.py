c661d32d2f7b1b521130a28ee30c2c35d5ef6f62
VIx-Lang <popolk1871@outlook.com>
2026-05-09 18:59:12 +0800

    项目重命名到xpm

---
+++ build/lib/cmds/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+import argparse
+
+
+class Command(ABC):
+    NAME: str
+
+    @property
+    def name(self) -> str:
+        # 如果报错了，看看你是不是没有定义 NAME
+        return self.NAME
+
+    def __init__(self, p: argparse._SubParsersAction):
+        self.parser = self.set_parser(p)
+        # 这里不立即解析参数，参数在 main.py 中统一解析
+        self.namespace = None
+
+    @abstractmethod
+    def set_parser(self, p: argparse._SubParsersAction) -> argparse.ArgumentParser: ...
+
+    @abstractmethod
+    def execute(self): ...
